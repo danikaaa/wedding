8507136c101fd164d2a529d0d949b69a94f14245
danikaaa <dltndus9563@naver.com>
2025-06-11 23:19:25 +0900

    틀잡기

---
+++ src/Account.js
@@ -0,0 +1,14 @@
+const Account = () => {
+    return(
+        <div>
+            <span>마음 전하실 곳</span>
+        <pre>참석이 어려우신 분들을 위해 기재했습니다.<br />
+          너그러운 마음으로 양해 부탁드립니다.
+        </pre>
+        <div>신랑측에게</div>
+      
+        <div>신부측에게</div>
+        </div>
+    )
+}
+export default Account
--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import Account from './Account';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='main'>
+      <div className='music'>음악</div>
+      <div className='img' >대문사진
+      <img className='mainImg' src={`${process.env.PUBLIC_URL}/img/main_img.JPG`}></img>
+      </div>
+      <div className='date'>
+        <pre>2025년 11월 8일 토요일, 오전 10시 50분</pre>
+        <pre>엔타워컨벤션웨딩홀, 1층 베일리홀</pre>
+      </div>
+      <div className='text'>
+        <pre>
+        캠퍼스에서 시작된 사랑이<br />
+        다섯 해를 지나 결실을 맺었습니다.<br />
+        이제 부부로서 첫걸음을 내딛고자 하오니,<br />
+        소중한 분들의 따뜻한 축복으로<br />
+        함께해 주시면 큰 기쁨이 되겠습니다.<br />
+        </pre>
+      </div>
+      <div className='img2'>이뮈쥐</div>
+      <div className='calendar'>달력</div>
+      <div className='gallery'>갤러리</div>
+      <div className='calendar'>달력
+        <div className='dday'>디데이</div>
+      </div>
+      <div className='parents'>
+        <b>윤 근 용 · 김 해 영</b> 의 장남   <b>윤 기 섭</b><br />
+        <b>이 영 호 · 송 영 화</b> 의 차녀   <b>이 수 연</b>
+        <div className='tel'>
+          <div className='telBtn'>
+            축하 연락하기
+          </div>
+        </div>
+      </div>
+      <div className='map'>지도
+      </div>
+      <div className='account'>
+      <Account/>
+      </div>
+
     </div>
+    
+
   );
 }
 
